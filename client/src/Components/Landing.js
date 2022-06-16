56b183a602b573b8d9db57c30407b8c586d7cdfb
Jun <jun279r@gmail.com>
2022-06-16 14:28:51 +0200

    Add fetch get request to my events

--- client/src/Components/Landing.js
+++ client/src/Components/Landing.js
@@ -1,4 +1,37 @@
+import { useEffect, useState } from "react";
+
 const Landing = () => {
+  const [events, setEvents] = useState(null);
+  const [loading, setLoading] = useState(false);
+  const { REACT_APP_EB_TOKEN, REACT_APP_ORG_ID } = process.env;
+
+  //retrieve events at render
+  useEffect(() => {
+    setLoading(true);
+    fetchEvents();
+    setLoading(false);
+  }, []);
+
+  const fetchEvents = async () => {
+    try {
+      const settings = {
+        method: "GET",
+        headers: {
+          "Content-Type": "application/json",
+          Authorization: `Bearer ${REACT_APP_EB_TOKEN}`,
+        },
+      };
+      const response = await fetch(
+        `https://www.eventbriteapi.com/v3/organizations/${REACT_APP_ORG_ID}/events/`,
+        settings
+      );
+      const data = await response.json();
+      setEvents(data.events);
+    } catch (error) {
+      console.log(error);
+    }
+  };
+
   return (
     <div>
       <h1>EventBright</h1>
