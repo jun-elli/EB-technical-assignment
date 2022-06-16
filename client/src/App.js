a575fc3aa83c99030f91cbdd8c3c33cb1cd05272
Jun <jun279r@gmail.com>
2022-06-16 12:05:35 +0200

    Add react router

--- client/src/App.js
+++ client/src/App.js
@@ -1,7 +1,15 @@
 import "./App.css";
+import { BrowserRouter, Routes, Route } from "react-router-dom";
+import Landing from "./Components/Landing";
 
 function App() {
-  return <div className="App">Hello World!</div>;
+  return (
+    <BrowserRouter>
+      <Routes>
+        <Route path="/" element={<Landing />} />
+      </Routes>
+    </BrowserRouter>
+  );
 }
 
 export default App;
---
+++ client/src/Components/Landing.js
@@ -0,0 +1,10 @@
+const Landing = () => {
+  return (
+    <div>
+      <h1>EventBright</h1>
+      <div>List of events</div>
+    </div>
+  );
+};
+
+export default Landing;
